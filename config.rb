7591162b3d344a3419987f294f3908e812fc807a
Maksym <max.domarev@gmail.com>
2019-06-21 18:33:35 +0300

    Update config.rb

--- config.rb
+++ config.rb
@@ -52,7 +52,7 @@ end
 
 # Deploy Configuration
 # If you want Middleman to listen on a different port, you can set that below
-set :port, 4567
+set :port, 80
 
 helpers do
   require './lib/toc_data.rb'
